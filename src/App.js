2f5cd76674747041fc4574a5726b93e53dedafdf
Muaz <muaz46@yahoo.com>
2019-01-22 22:44:35 +0800

    add table and centre them

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import List from './List'
 import Textbox from './TextBox/Textbox'
-import Button from './Button/Button'
 import ButtonWithoutBind from './Button/ButtonWithoutBind/ButtonWithoutBind'
+import Table from './Table/table'
 
 class App extends Component {
   constructor (props) {
     super(props)
-      console.log(this.props)
-        this.state = {
+    console.log(this.props)
+    this.state = {
       num: 0,
       name: ''
 
@@ -20,8 +19,8 @@ class App extends Component {
     this.setState({
       num: this.state.num + 1
 
-    })  
-   }
+    })
+  }
 
    saveValue = (event) => {
      this.setState({ name: event.target.value })
@@ -32,33 +31,45 @@ class App extends Component {
    }
 
    render () {
-     const { name } = this.state.name 
+     const { name } = this.state.name
+     const details = [
+       {
+         'name': 'Amri Yahya',
+         'details': 'amri@yahoo.com'
+       },
+       {
+         'name': 'Mr Brian',
+         'details': 'brian@yahoo.com'
+       },
+       {
+         'name': 'Najib Bossku',
+         'details': 'najib@mastermind.com'
 
-    return (
-       <div className='App'>
-         <header className='App-header'>
-           <img src={logo} className='App-logo' alt='logo' />
-           <p>
-             <List title='New Project' user='Test Posting Data' />
-            <List user='Initial Project' />
-          </p>
-           <Textbox text='Reassign Data' value='13' />
+       }
+     ]
+     return (
+      <div className='App'>
+         {/* <header className='App-header'> */}
+         <img src={logo} className='App-logo' alt='logo' />
+         {/* </header> */}
 
-          <br />
-          {this.state.num}
-           <button onClick={this.incrementNum}>Add</button>
-           <Button button='Reset' />
-           <ButtonWithoutBind />
-           {this.state.name}
+         <div className='container' style={ { display: 'flex', justifyContent: 'center', alignItems: 'center' } }>
+           <Table details={details} />
+         </div>
 
-           <form>
-             <input type='text' value={this.state.name} name='testing' placeholder={name || null} onChange={this.saveValue} />
-             <button type='submit' onClick='{this.showValue}'>Enter</button>
-           </form>
-         </header>
+         <Textbox text='Reassign Data' value='13' />
+         <br />
+         {this.state.num}
+         <button onClick={this.incrementNum}>Add</button>
+         <ButtonWithoutBind />
+         {this.state.name}
+
+         <input type='text' value={this.state.name} name='testing' placeholder={name || null} onChange={this.saveValue} />
+         <button type='submit' onClick='{this.showValue}'>Enter</button>
+
        </div>
      )
-  }
+   }
 }
 
 export default App
